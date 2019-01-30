625c4ebf9545317bfab513ee88f70f55a6317a4d
Rovexx <roel.versteeg@hotmail.com>
2019-01-30 22:18:23 +0100

    Add product page added

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,6 +11,11 @@
                 <v-icon left>view_list</v-icon>
                 <router-link to='/products' ><span class="link2 mr-2">Producten overzicht</span></router-link>
             </v-btn>
+            <p class="devider blue--text text--darken-1"> | </p>
+            <v-btn flat>
+                <v-icon left>playlist_add</v-icon>
+                <router-link to='/add' ><span class="link2 mr-2">Product toevoegen</span></router-link>
+            </v-btn>
         </v-toolbar>
     </nav>
 </template>
@@ -28,5 +33,10 @@ export default {
 .link2{
     color: white;
     font-size: 1.2em;
+}
+
+.devider{
+    font-size: 3em;
+    margin: 0px;
 }
 </style>
--- src/views/AddProduct.vue
+++ src/views/AddProduct.vue
@@ -1,65 +1,97 @@
 <template>
   <v-container>
-    <v-form if="!submitted" ref="form" v-model="valid" lazy-validation>
-      <v-text-field v-model="name" :rules="nameRules" label="Name" required></v-text-field>
+    <v-layout align-center justify-center>
+      <v-flex md8 elevation-1>
+        <!-- Title -->
+        <div class="grey darken-3">
+          <h1 class="detail font-weight-light white--text">Product toevoegen</h1>
+        </div>
+        <!-- form -->
+        <v-container class="grey lighten-4 category">
+          <v-form if="!submitted" ref="form" v-model="valid" lazy-validation>
+            <v-text-field v-model="name" :rules="nameRules" label="Naam" required></v-text-field>
+            <v-text-field v-model="brand" :rules="nameRules" label="Merk" required></v-text-field>
+            <v-text-field v-model="spoiled" :rules="nameRules" label="Over de datum (true | false)" required></v-text-field>
 
-      <v-text-field v-model="brand" :rules="nameRules" label="Brand" required></v-text-field>
+            <v-btn :disabled="!valid" color="primary" v-on:click.prevent="post">Add</v-btn>
 
-      <v-text-field v-model="spoiled" :rules="nameRules" label="Spoiled" required></v-text-field>
+            <v-btn color="error" @click="reset">Reset Form</v-btn>
+          </v-form>
+          <div v-if="submitted">Product toegevoegd!</div>
 
-      <v-btn :disabled="!valid" color="success" @click="validate">Validate</v-btn>
-      <v-btn :disabled="!valid" color="primary" v-on:click.prevent="post">Add</v-btn>
-
-      <v-btn color="error" @click="reset">Reset Form</v-btn>
-      <v-btn color="warning" @click="resetValidation">Reset Validation</v-btn>
-    </v-form>
-    <div v-if="submitted">Thanks for posting a product!</div>
+          <!-- Back button -->
+          <v-btn class="grey darken-4" style="cursor: pointer;" v-on:click="navigate()">
+            <a class="back">Terug</a>
+          </v-btn>
+        </v-container>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
+import router from '../router'
+import axios from "axios";
+
 export default {
-  data: () => ({
-    valid: true,
-    name: "",
-    brand: "",
-    spoiled: "",
-    nameRules: [
-      v => !!v || "required!",
-    ],
-    submitted: false
-  }),
-  
-  methods: {
-    validate() {
-      if (this.$refs.form.validate()) {
-        this.snackbar = true;
-      }
-    },
-    reset() {
-      this.$refs.form.reset();
-    },
-    resetValidation() {
-      this.$refs.form.resetValidation();
-    },
+  name: 'ProductDetails',
+  data(){
+    return{
+      valid: true,
+      name: "",
+      brand: "",
+      spoiled: "",
+      nameRules: [
+        v => !!v || "required!"
+      ],
+      submitted: false
+    }
+  },
+  created(){
+  },
+  methods:{
     post: function() {
       const axios = require("axios");
+      var headers = {
+        'content-type': 'application/JSON'
+      }
       axios
         .post("http://server.arvex.nl/api/products/", {
+
           name: this.namepost,
           brand: this.brandpost,
           spoiled: this.spoiledpost
         })
         .then(function(response) {
           console.log(response);
+          // empty form
+          self.namepost = "";
+          self.brandpost = "";
+          self.spoiledpost = "";
         })
         .catch(function(error) {
           console.log(error);
         });
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+    navigate(){
+      router.go(-1);
     }
   }
 };
 </script>
 
 <style>
+.category p{
+  display: inline-block;
+  margin: 10px 0;
+}
+
+.detail{
+  margin-left: 10px;
+  text-decoration: underline;
+  text-decoration-color:rgb(28, 124, 235);
+}
 </style>
